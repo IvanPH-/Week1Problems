14688ff4d3b44bef657fbb9adfbdbac24cef1872
IvanPH- <Iperryhavens@hotmail.com>
2015-03-27 18:57:14 -0500

    Changed arrays to lists. Added method to check for pairs

--- P6.java
+++ P6.java
@@ -1,6 +1,9 @@
 package week1Problems;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class P6 {
 	/* ### Poker Hands
@@ -11,37 +14,54 @@ Part 2: Imagine that there was a third player. Generate a possible hand for the 
 Part 3: If we look at pokernew.txt instead of poker.txt, how many hands does player1 win now? Modify p6a.java from Part 1 to ask the user for an input (ask if there are 2 or 3 players in the game). Print the number of games Player 1 wins in each situation to the console. Save this modified code as p6c.java in the answers directory.
 	 */
 	public static void main(String[] args) {
-		int[] a = {2, 14, 14, 12, 8};
-		int[] b = {5, 14, 13, 2, 12};
-		Arrays.sort(a);
-		Arrays.sort(b);
+		List<Integer> a = new ArrayList<>();
+		a.add(2);
+		a.add(14);
+		a.add(14);
+		a.add(12);
+		a.add(8);
+		Collections.sort(a);
+		
+		List<Integer> b = new ArrayList<>();
+		b.add(5);
+		b.add(15);
+		b.add(15);
+		b.add(2);
+		b.add(12);
+		Collections.sort(b);
 		boolean[] player1Boolean = new boolean[8];
 		boolean[] player2Boolean = new boolean[8];
 		compare(a, b, player1Boolean, player2Boolean);
 	}
 	
-	public static void compare(int[] x, int[] y, boolean[] a, boolean[] b) {
-		checkPair(x, a);
-		checkPair(y, b);
+	public static void compare(List<Integer> x, List<Integer> y, boolean[] a, boolean[] b) {
+		checkPairSeries(x, a);
+		checkPairSeries(y, b);
 		compareHands(x, y, a, b);
 		//Called after comparing both hands values if neither passes in another method somewhere compareHigh(x, y);
 	}
 	
-	private static void compareHands(int[] x, int[] y, boolean[] a, boolean[] b) {
-		
-		
-	}
-
-	private static boolean checkPair(int[] x, boolean[] y) {
-		for(int i = 0; i < x.length - 1; i++) {
-			if(x[i] == x[i + 1]) {
-				return y[0] = true;
+	private static void checkPairSeries(List<Integer> x, boolean[] y) {
+		List<Integer> hold = new ArrayList<>();
+		for(int i = 0; i <= x.size() - 2; i++) {
+			if(x.get(i) == x.get(i + 1)) {
+				if(y[0] == true) {
+					y[0] = false;
+					y[1] = true;
+				}
+				hold.add(x.get(i));
+				hold.add(x.get(i + 1));
+				x.remove(x.get(i));
+				x.remove(x.get(i));
+				y[0] = true;
 			}
 		}
-		return y[0] = false;
+		x.addAll(hold);
+		Collections.sort(x);
+		hold.clear();
 	}
 		
-	private static void compareHigh(int[] x, int[] y) {
+	private static void compareHigh(List<Integer> x, List<Integer> y) {
 		int player1High = findHighCard(x);
 		int player2High = findHighCard(y);
 		if (player1High > player2High) {
@@ -55,13 +75,13 @@ Part 3: If we look at pokernew.txt instead of poker.txt, how many hands does pla
 		}
 	}
 
-	private static void compareNextHigh(int[] x, int[] y) {
-		for(int i = x.length - 2; i >= 0; i--) {
-			if (x[i] > y[i]) {
+	private static void compareNextHigh(List<Integer> x, List<Integer> y) {
+		for(int i = x.size() - 2; i >= 0; i--) {
+			if (x.get(i) > y.get(i)) {
 				System.out.print("Player 1 wins");
 				break;
 			}
-			else if (x[i] == y[i]) {
+			else if (x.get(i) == y.get(i)) {
 				continue;
 			}
 			else {
@@ -72,9 +92,33 @@ Part 3: If we look at pokernew.txt instead of poker.txt, how many hands does pla
 		
 	}
 
-	private static int findHighCard(int[] x) {
-		int highNum = x.length - 1;
-		return x[highNum];
+	private static int findHighCard(List<Integer> x) {
+		int highNum = x.size() - 1;
+		return x.get(highNum);
+	}
+
+	private static void compareHands(List<Integer> x, List<Integer> y, boolean[] a, boolean[] b) {
+		int buildValue1 = findValue(a);
+		int buildValue2 = findValue(b);
+		if(buildValue1 > buildValue2) {
+			System.out.print("Player 1 wins");
+		}
+		else if(buildValue1 == buildValue2) {
+			compareHigh(x, y);
+		}
+		else {
+			System.out.print("Player 2 wins");
+		}
 	}
 	
+	private static int findValue(boolean[] x) {
+		int value = 0;
+		for(int i = 0; i <= x.length - 1; i++) {
+			if (x[i] == true) {
+				value += i + 1;
+				break;
+			}
+		}
+		return value;
+	}
 }
